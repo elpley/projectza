95cdffaf678d8741f3e7245bf50c9b158e028d95
Suratchana Pinthong <58160328@go.buu.ac.th>
2017-12-01 19:45:19 +0700

    test 5

--- contacts/index.js
+++ contacts/index.js
@@ -3,22 +3,38 @@ const contacts = express.Router();
 
 var contact1 = [
     {
-        id:1,
-        First_Name:'Nonthawat',
-        Last_Name:'Kwankao',
+        id: 1,
+        First_Name: 'Nonthawat',
+        Last_Name: 'Kwankao',
         email: '58160466@go.buu.ac.th'
     },
     {
-        id:2,
-        First_Name:'Suratchana',
-        Last_Name:'Pinthong',
+        id: 2,
+        First_Name: 'Suratchana',
+        Last_Name: 'Pinthong',
         email: '58160328@go.buu.ac.th'
     },
     {
-        id:3,
-        First_Name:'Saranya',
-        Last_Name:'Sarakun',
+        id: 3,
+        First_Name: 'Saranya',
+        Last_Name: 'Sarakun',
         email: '58160316@go.buu.ac.th'
     }
 ]
+
+contacts.get('/contacts/:name', (req, res) => {
+    let friend = req.body
+    let index = contact1.findIndex((index) => {
+        if (friend.First_Name == contact1.First_Name) {
+            return index
+        }
+    })
+    res.json(contact1[index])
+
+
+})
+
 module.exports = contacts
+
+
+
